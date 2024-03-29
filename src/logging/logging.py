0f71b52385080efdc42216a164b4bb6a5fd1eb25
Dmitry <dimic0893@gmail.com>
2024-03-29 17:17:19 +0400

    ADD: Dockerfile

--- src/logging/logging.py
+++ src/logging/logging.py
@@ -1,18 +1,27 @@
 import logging
+import os
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
-# Создаем обработчик для записи в файл
-file_handler = logging.FileHandler('logger.log')
-file_handler.setLevel(logging.INFO)
+class UserLogger:
+    def __init__(self, user_id):
+        self.logger = logging.getLogger(__name__ + '.' + str(user_id))
+        self.logger.setLevel(logging.INFO)
 
-# Создаем форматтер
-formatter = logging.Formatter('%(asctime)s - [%(levelname)s] - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        # Создаем отдельный файл для каждого пользователя
+        log_file = f'src/logger-users/{str(user_id)}_logger.log'
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.INFO)
 
-# Назначаем форматтер обработчику
-file_handler.setFormatter(formatter)
+        formatter = logging.Formatter('%(asctime)s - [%(levelname)s] - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        file_handler.setFormatter(formatter)
 
-# Добавляем обработчик к логгеру
-logger.addHandler(file_handler)
+        self.logger.addHandler(file_handler)
 
+    def log_info(self, message):
+        self.logger.info(message)
+
+    def log_error(self, message):
+        self.logger.error(message)
+
+    def log_warning(self, message):
+        self.logger.warn(message)
--- src/logging/router.py
+++ src/logging/router.py
@@ -1,23 +1,30 @@
-from typing import Optional
+import os
+from typing import Optional, Dict
 
 from fastapi import APIRouter, Request
+from src.logging.logging import UserLogger
+import logging
 
-from src.logging.logging import logger
 
 router = APIRouter(
     prefix="",
     tags=["Logging"]
 )
 
+user_loggers: Dict[int, UserLogger] = {}
 
 @router.get("/logger")
-async def write_logging(type: str, message: str):
+async def write_logging(type: str, message: str, user_id: int):
+    print(user_loggers)
+    if user_id not in user_loggers:
+        user_loggers[user_id] = UserLogger(user_id)
+    user_logger = user_loggers[user_id]
     if type == "ERROR":
-        logger.error(message)
+        user_logger.log_error(message)
     if type == "INFO":
-        logger.info(message)
+        user_logger.log_info(message)
     if type == "WARNING":
-        logger.warning(message)
+        user_logger.log_warning(message)
 
 
 @router.post("/logger_frontend")
@@ -26,14 +33,25 @@ async def write_logging(request: Request):
         data = await request.json()
         type = data.get("type")
         message = data.get("message")
+        user_id = int(data.get("user_id"))
+        if user_id not in user_loggers:
+            user_loggers[user_id] = UserLogger(user_id)
+        user_logger = user_loggers[user_id]
         if type == "ERROR":
-            logger.error(message)
+            user_logger.log_error(message)
         if type == "INFO":
-            logger.info(message)
+            user_logger.log_info(message)
         if type == "WARNING":
-            logger.warning(message)
+            user_logger.log_warning(message)
+
         return {"status": "success"}
     except Exception as e:
         return {"status": 500, "message": str(e)}
+
+
+
+
+
+
 
 
